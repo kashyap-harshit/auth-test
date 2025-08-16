ff41447c5abd9fcb68542e12aa9474d8f62be33b
Harshit <kashyapharshit50@gmail.com>
2025-08-16 22:24:31 +0530

    added app/login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import LoginForm from '@/components/LoginForm'
+import React from 'react'
+
+function Login() {
+  return (
+    <div className='w-full flex justify-center h-[100vh] items-center'>
+      <LoginForm/>
+    </div>
+  )
+}
+
+export default Login
